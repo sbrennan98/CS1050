d886f5645a9068371aa0ce3162913839c1f86098
Sean Brennan <brennansean6@gmail.com>
2019-02-17 19:14:06 -0600

    .

---
+++ Switch&parameters.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int function();
+
+int main(void)
+{
+	function();
+	return 0;
+}
+
+int function()
+{
+	
+}
---
+++ field-width.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a=3;
+	double b=3.25;
+	printf("A= Value of B=\n%3d%12.2f", a, b);
+}
